5bcb73b62f121212bd0e65de84bfef8db4ba2228
Alaina-gl <108637123+Alaina-gl@users.noreply.github.com>
2024-10-13 00:45:52 -0700

    new-group-header

--- decider-ui/app/new-group.tsx
+++ decider-ui/app/new-group.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
+
+const { width, height } = Dimensions.get('window')
 
 const NewGroup = () => {
   return (
-    <View>
-      <Text>NewGroup</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>NewGroup</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start', // Aligns items at the top
+    alignItems: 'center',
+    paddingTop: height * 0.07, // 5% of screen height
+  },
+  title: {
+    fontSize: width * 0.05, // 5% of screen width
+    fontWeight: 'bold',
+  },
+})
 
 export default NewGroup
